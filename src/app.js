ef6797e702b185f3a36b1d4f1d1684550dac09d7
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:41:03 -0400

    registered the post reader component

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const NavigationComponent = require('./components/navigation');
 const HomeComponent = require('./components/home');
 const LoginComponent = require('./components/login');
 const SignupComponent = require('./components/signup');
+const PostReaderComponent = require('./components/post-reader');
 
 angular.module('bloggo', [])
       .component('home', HomeComponent)
       .component('navigation', NavigationComponent)
       .component('login', LoginComponent)
-      .component('signup', SignupComponent);
+      .component('signup', SignupComponent)
+      .component('postReader', PostReaderComponent);
