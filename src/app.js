21906086c46f78ff245fc691b42d07827f540442
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:28:11 -0400

    registered the signup component

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ const angular = require('angular');
 const NavigationComponent = require('./components/navigation');
 const HomeComponent = require('./components/home');
 const LoginComponent = require('./components/login');
-
+const SignupComponent = require('./components/signup');
 
 angular.module('bloggo', [])
       .component('home', HomeComponent)
       .component('navigation', NavigationComponent)
-      .component('login', LoginComponent);
+      .component('login', LoginComponent)
+      .component('signup', SignupComponent);
