52c689b7173964b0dff7f546b34a327d412bde57
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:54:28 -0400

    registered the post-editor component

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const HomeComponent = require('./components/home');
 const LoginComponent = require('./components/login');
 const SignupComponent = require('./components/signup');
 const PostReaderComponent = require('./components/post-reader');
+const PostEditorComponent = require('./components/post-editor');
 
 angular.module('bloggo', [])
       .component('home', HomeComponent)
       .component('navigation', NavigationComponent)
       .component('login', LoginComponent)
       .component('signup', SignupComponent)
-      .component('postReader', PostReaderComponent);
+      .component('postReader', PostReaderComponent)
+      .component('postEditor', PostEditorComponent);
