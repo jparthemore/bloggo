8732446845fa39c92342e04b6f8cbd124daab192
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:40:31 -0400

    created the post-reader component definition

---
+++ src/components/post-reader/index.js
@@ -0,0 +1,9 @@
+const controller = require('./post-reader.controller');
+const template = require('./post-reader.html');
+
+const component = {
+  controller,
+  template
+};
+
+module.exports = component;
