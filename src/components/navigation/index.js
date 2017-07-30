be4fecdf0effc831bfa33861ac1413f5004cd276
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 17:45:34 -0400

    created the definition of the component. Utilized the shorthand syntax of ES2015 to have the controller and template variables

---
+++ src/components/navigation/index.js
@@ -0,0 +1,10 @@
+const controller = require('./navigation.controller');
+const template = require('./navigation.html');
+
+
+const component = {
+  controller,
+  template
+}
+
+module.exports = component;
