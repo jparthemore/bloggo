491b8a2fd4e7d17a6abe649a68e2540a03c676e9
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:54:07 -0400

    created the component defintion for the post editor

---
+++ src/components/post-editor/index.js
@@ -0,0 +1,9 @@
+const controller = require('./post-editor.controller');
+const template = require('./post-editor.html');
+
+const component = {
+  controller,
+  template
+};
+
+module.exports = component;
