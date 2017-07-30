5f10ad0d637426a601d3a280dfb643c6836e305c
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:00:24 -0400

    registered the home component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const angular = require('angular');
 
 //component imports
 const NavigationComponent = require('./components/navigation');
+const HomeComponent = require('./components/home');
 
 angular.module('bloggo', [])
+      .component('home', HomeComponent)
       .component('navigation', NavigationComponent);
