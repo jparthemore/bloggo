525d1739cf6eaf508ddcd2f2f9760a963ef36871
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 18:14:34 -0400

    registered the login component with the application

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const angular = require('angular');
 //component imports
 const NavigationComponent = require('./components/navigation');
 const HomeComponent = require('./components/home');
+const LoginComponent = require('./components/login');
+
 
 angular.module('bloggo', [])
       .component('home', HomeComponent)
-      .component('navigation', NavigationComponent);
+      .component('navigation', NavigationComponent)
+      .component('login', LoginComponent);
