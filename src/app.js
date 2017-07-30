6fe814b7666387535299a9631801e300a1191e48
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 17:45:55 -0400

    registered the navigation component in the appication

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const angular = require('angular');
 
+//component imports
+const NavigationComponent = require('./components/navigation');
+
 angular.module('bloggo', [])
       .component('navigation', NavigationComponent);
