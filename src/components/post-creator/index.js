1c19a5fdc25730fe2a74fd000e6fc3689a0e3435
Justin Dennison <justin1dennison@gmail.com>
2017-07-30 19:08:01 -0400

    created the component definition for the create post component

---
+++ src/components/post-creator/index.js
@@ -0,0 +1,10 @@
+const controller = require('./post-creator.controller');
+const template = require('./post-creator.html');
+
+const component = {
+  controller,
+  template
+};
+
+
+module.exports = component;
